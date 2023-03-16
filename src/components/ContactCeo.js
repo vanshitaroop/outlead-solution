cd657bf610990d0183a7e68fb45c7a139b53154c
Vanshita Roopchandani <vanshita@Vanshitas-MacBook-Pro.local>
2023-03-16 10:18:01 +0530

    testing for mobile

--- src/components/ContactCeo.js
+++ src/components/ContactCeo.js
@@ -9,8 +9,8 @@ export const ContactCeo = () => {
             <div className='row'>
                 <div className='col-md-6 forCenter'>
                 <div class="card-container" style={{textAlign:"center"}}>
-                <div class="front">Front Side</div>
-                <div class="back">Back Side</div>
+                <div class="front" style={{color:"black"}}>Front Side</div>
+                <div class="back"  style={{color:"black"}}>Back Side</div>
                  </div>
                 </div>
                 <div className='col-md-6 forCenter'>
