377f235a196ec9da489944338eacd9627f4e9739
Vanshita Roopchandani <vanshita@Vanshitas-MacBook-Pro.local>
2023-04-06 16:15:53 +0530

    test

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,14 +1,16 @@
 import "./text.css";
 import React from 'react'
 import { Whatwedo } from "./Whatwedo";
-
+// import Slider from "react-slick";
+// import "~slick-carousel/slick/slick.css"; 
+// import "~slick-carousel/slick/slick-theme.css";
 export const Clients = () => {
     return (
         <>
          <div class="nine" style={{marginTop:"60px"}}>
         <h1><span><h2>Our Key Clients</h2></span></h1>
       </div>
-       {/* <div className="container">
+       <div className="container">
        <div class="containerClient " style={{paddingBottom:"120px"}}>
                 <div class="carousell">
                     <div class="carousel__facee"><span></span></div>
@@ -22,7 +24,12 @@ export const Clients = () => {
                     <div class="carousel__facee"><span></span></div>
                 </div>
             </div>
-       </div> */}
+       </div>
+       <div className="card">
+        <div className="card-top">
+            
+        </div>
+       </div>
        
         </>
     )
