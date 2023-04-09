31082f77589184c25072b9833d0037ce8e46b8b9
Vanshita Roopchandani <vanshita@192.168.1.4>
2023-04-09 19:22:37 +0530

    slides for testimonial added

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import  Carousel  from 'react-elastic-carousel';
+import Carousel from 'react-elastic-carousel';
 
 export const Testimonial = () => {
-  
-    
-      return (
+
+
+    return (
         <>
-         <Carousel enableAutoPlay autoPlaySpeed={2000} style={{padding:"120px"}}>
-      <div>
-        <h3>Slide 1</h3>
-      </div>
-      <div>
-        <h3>Slide 2</h3>
-      </div>
-      <div>
-        <h3>Slide 3</h3>
-      </div>
-      <div>
-        <h3>Slide 4</h3>
-      </div>
-    </Carousel>
-    </>
-  );
+            <Carousel enableAutoPlay autoPlaySpeed={2000} style={{ padding: "120px" }}>
+                <div>
+                    <h3>Testimonial 1</h3>
+                </div>
+                <div>
+                    <h3>Testimonial 2</h3>
+                </div>
+                <div>
+                    <h3>Testimonial 3</h3>
+                </div>
+                <div>
+                    <h3>Slide 4</h3>
+                </div>
+            </Carousel>
+        </>
+    );
 };
