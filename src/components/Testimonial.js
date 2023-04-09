9366229c28d3ee0a594648fc3cf7fdae714fec5d
Vanshita Roopchandani <vanshita@192.168.1.4>
2023-04-09 20:22:41 +0530

    test for carousel .

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import Carousel from 'react-elastic-carousel';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 export const Testimonial = () => {
-
+    const items = [
+        <div key={1} className="item"><h1>Item 1</h1></div>,
+        <div key={2} className="item"><h1>Item 2</h1></div>,
+        <div key={3} className="item"><h1>Item 3</h1></div>,
+        <div key={4} className="item"><h1>Item 4</h1></div>,
+        <div key={5} className="item"><h1>Item 5</h1></div>,
+      ];
+      
 
     return (
         <>
-            <Carousel enableAutoPlay autoPlaySpeed={2000} style={{ padding: "120px" }}>
-                <div>
-                    <h3>Testimonial 1</h3>
-                </div>
-                <div>
-                    <h3>Testimonial 2</h3>
-                </div>
-                <div>
-                    <h3>Testimonial 3</h3>
-                </div>
-                <div>
-                    <h3>Slide 4</h3>
-                </div>
-            </Carousel>
-        </>
+         <AliceCarousel
+         style={{padding:"120px",backgroundColor:"white"}}
+      autoPlay
+      infinite
+      autoPlayInterval={2000}
+      buttonsDisabled={true}
+      items={items}
+      responsive={{
+        0: { items: 1 },
+        768: { items: 3 },
+        1024: { items: 5 },
+      }}
+    />
+    </>
     );
 };
