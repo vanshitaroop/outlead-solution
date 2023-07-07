31c027b376198be87c22641a651880eaa142d0ff
vanshita Roopchandani <vanshitaroopchandani7@gmail.com>
2023-07-08 00:17:01 +0530

    Updated about us page

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,19 @@ export const About = () => {
       <hr className='abouthr'/>
       </div>
       <div className='contentofAbout mx-4 my-3'>
-        <p>
+        {/* <p>
         Lorem ipsum dolor sit amet,  <br/>consectetur adipiscing elit, <br/>sed do eiusmod tempor incididunt ut labore<br/>Ut enim ad minim veniam, quis 
         </p>
         <p>
         Lorem ipsum dolor sit amet,  
-        </p>
+        </p> */}
+        <p style={{paddingLeft:"0px"}}>
+        We are an innovative marketing company, <br/>
+        dedicated to helping businesses succeed through 
+        <br/>tailored strategies and captivating campaigns. 
+        <br/>Together, we unlock our clients' 
+       potential in today's competitive marketplace.
+</p>
         <button type="button" class="GAQ">Text us! <i class="fa fa-paper-plane"></i></button>
       </div>
     </div>
